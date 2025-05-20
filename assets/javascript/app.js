f0789281342615cf4827b5f487d253edf50034df
Veksoe <Anniika-xb@live.dk>
2025-05-20 11:35:18 +0200

    Test
    Opstart på hvem er vi

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,17 @@
 let prevScrollpos = window.pageYOffset;
+const bodyEl = document.querySelector("body");
 const mobilMenuBtnEl = document.querySelector(".mobil-menu");
 const mobilNavEl = document.querySelector(".mobil-nav");
 const mobilMenuCloseEl = document.querySelector(".mobil-menu-close");
 
 mobilMenuBtnEl.addEventListener("click", () => {
     mobilNavEl.style.transform = "translateX(0px)"
+    bodyEl.style.overflow = "hidden";
 })
 mobilMenuCloseEl.addEventListener("click", () => {
     mobilNavEl.style.transform = "translateX(500px)"
+    bodyEl.style.overflow = "visible";
+
 })
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
