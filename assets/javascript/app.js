5c4d92bd890a8de7737873fb0144c6efa61af811
Veksoe <Anniika-xb@live.dk>
2025-05-22 11:01:31 +0200

    Mobil login

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -86,4 +86,28 @@ function showSliders(n) {
 }
 
 
+// Get the modal
+var modal = document.querySelector("#myModal");
 
+// Get the button that opens the modal
+var btn = document.querySelector("#myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".modal-close")
+
+// When the user clicks on the button, open the modal
+btn.addEventListener('click', () => {
+    modal.style.display = "block";
+})
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener('click', () => {
+    modal.style.display = "none";
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+})
